3a4784c22dc4d233eb69f3ee3c53569041ba5f68
KESAVAGAJJALA <115894976+KESAVAGAJJALA@users.noreply.github.com>
2024-02-04 21:30:45 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "kesh" # Replace with your actual S3 bucket name
-    key    = "terraform.tfstate"
+    key    = "EKS-terraform.tfstate"
     region = "eu-west-2"
   }
 }
